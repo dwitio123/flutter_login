4bd1884ecff5ba53dedefd9dd3d11a913d3a2d73
dwitio123 <tiothebook@gmail.com>
2024-11-30 09:39:43 +0700

    first commit

---
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+enum AuthenticationStatus { unknown, authenticated, unathenticated }
+
+class AuthenticationRepository {
+  final _controller = StreamController<AuthenticationStatus>();
+
+  Stream<AuthenticationStatus> get status async* {
+    await Future<void>.delayed(const Duration(seconds: 1));
+    yield AuthenticationStatus.unathenticated;
+    yield* _controller.stream;
+  }
+
+  Future<void> logIn({
+    required String username,
+    required String password,
+  }) async {
+    await Future.delayed(
+      const Duration(milliseconds: 300),
+      () => _controller.add(AuthenticationStatus.authenticated),
+    );
+  }
+
+  void logOut() {
+    _controller.add(AuthenticationStatus.unathenticated);
+  }
+
+  void dispose() => _controller.close();
+}
